673e5ec87e027095751710a6130a37b12f9ee9ca
Danila <vereshagindanila2007@gmail.com>
2024-11-27 19:03:57 +0300

    CircleComplited
    hi dmitri

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import sys
 import random
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPlainTextEdit
+from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QPainter, QColor
-from circle_ui import Ui_Form
+from PyQt6 import uic
 
 
 class CircleWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi("UI.ui", self)
         self.radius = 0
-        self.ui.pushButton.clicked.connect(self.generate_circle)
+        self.createCircleButton.clicked.connect(self.generate_circle)
         self.setWindowTitle("Генерация окружностей")
         self.setGeometry(100, 100, 400, 400)
+        self.radii = []
 
     def generate_circle(self):
-        self.radius = random.randint(10, 100)
+        radius = random.randint(10, 100)
+        self.radii.append(radius)
         self.update()
 
-    def paintCircle(self, event):
+    def paintEvent(self, event):
         painter = QPainter(self)
         painter.setBrush(QColor(255, 255, 0))
         width = self.width()
@@ -27,10 +31,14 @@ class CircleWidget(QWidget):
         x = (width - self.radius) // 2
         y = (height - self.radius) // 2
         painter.drawEllipse(x, y, self.radius, self.radius)
+        for radius in self.radii:
+            x = (self.width() - radius) // 2  # Центрируем окружность
+            y = (self.height() - radius) // 2
+            painter.drawEllipse(x, y, radius, radius)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = CircleWidget()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
